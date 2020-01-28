07f2e52efdc1cd10f7687dbf9a25cc97d3f54386
Emanuele <emanuele.drago@outlook.it>
2020-01-28 13:30:41 +0100

    Update Main.java

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -31,6 +31,7 @@ public class Main extends Application {
 
 		stage.setTitle("Ruota della Fortuna");
 
+		
 		loader = new FXMLLoader(getClass().getResource("Login.fxml"));
 		//avviamo la prima finestra, Login
 		Parent root = loader.load();
